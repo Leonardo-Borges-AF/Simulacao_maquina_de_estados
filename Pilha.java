26482ba555cabeb1860663489ee83098c0d5d21d
Leonardo Borges <78118687+Leonardo-Borges-AF@users.noreply.github.com>
2023-01-31 19:56:30 -0300

    Add files via upload

---
+++ Pilha.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Pilha {
+    public void inciar(){
+
+    }
+}
